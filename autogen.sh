fc54b84671927f9c4ce20707309c92b0e1f8c13a
Nick Dyer <nick.dyer@itdev.co.uk>
2012-06-19 15:24:26 +0100

    improve handling of CFLAGS

--- autogen.sh
+++ autogen.sh
@@ -3,13 +3,6 @@
 # Remove dependency cache otherwise file renames confuse autoconf
 find . -type d -name \.deps | xargs rm -rf
 
-OBPCFLAGS="-g -Wall \
--Wmissing-declarations -Wmissing-prototypes \
--Wnested-externs -Wpointer-arith \
--Wsign-compare -Wchar-subscripts \
--Wstrict-prototypes -Wshadow \
--Wformat-security -Wtype-limits"
-
 autoreconf -v --install
 
-./configure CFLAGS="${OBPCFLAGS} ${CFLAGS}" $@
+./configure $@
