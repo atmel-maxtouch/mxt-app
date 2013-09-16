0f118ce5e812738a855e6f835005e01c4fdf6dad
Nick Dyer <nick.dyer@itdev.co.uk>
2013-09-16 11:46:51 +0100

    Fix missing header in usb_device.c

--- src/libmaxtouch/usb/usb_device.c
+++ src/libmaxtouch/usb/usb_device.c
@@ -28,6 +28,7 @@
 //------------------------------------------------------------------------------
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <inttypes.h>
 #include <string.h>
