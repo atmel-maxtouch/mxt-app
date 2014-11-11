306a58dc6aa7296507ba0791ac520fe608270635
Nick Dyer <nick.dyer@itdev.co.uk>
2014-11-11 10:13:40 +0000

    Do not allocate dmesg buffer on stack
    Signed-off-by: Nick Dyer <nick.dyer@itdev.co.uk>

--- src/libmaxtouch/sysfs/dmesg.c
+++ src/libmaxtouch/sysfs/dmesg.c
@@ -110,7 +110,7 @@ static void dmesg_list_empty(struct mxt_device *mxt)
 /// \return #mxt_rc
 int sysfs_get_msg_count(struct mxt_device *mxt, int *count)
 {
-  char buffer[KLOG_BUF_LEN + 1];
+  static char buffer[KLOG_BUF_LEN + 1];
   char msg[BUFFERSIZE];
   char *msgptr;
   char *lineptr;
