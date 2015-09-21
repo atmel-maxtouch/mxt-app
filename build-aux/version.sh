f29b106dbb97d7ad1946cf0aa01894ecdf08ec89
Nick Dyer <nick.dyer@itdev.co.uk>
2015-09-21 16:43:05 +0100

    Fix shellcheck issues in build-aux/version.sh
    Signed-off-by: Nick Dyer <nick.dyer@itdev.co.uk>

--- build-aux/version.sh
+++ build-aux/version.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-TOPDIR=$(dirname $0)/..
+TOPDIR=$(dirname "$0")/..
 
 if [ -d "$TOPDIR/.git" ]
 then
-  VERSION=`cd $TOPDIR; git describe --dirty=-mod --always | sed s/^v//`
+  VERSION=$(cd "$TOPDIR" && git describe --dirty=-mod --always | sed s/^v//)
 fi
 
-if [ -z $VERSION ]
+if [ -z "$VERSION" ]
 then
-  VERSION=`tr -d '\n' < $TOPDIR/VERSION`
+  VERSION=$(tr -d '\n' < "$TOPDIR/VERSION")
 fi
 
-echo $VERSION
+echo "$VERSION"
