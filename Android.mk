586b64efed9b856e0fc23f4ec047f6c2b9d4a8a4
Bowe Neuenschwander <bowe.neuenschwander@garmin.com>
2018-08-31 20:56:30 -0500

    Fix AOSP build
    The top-level Android.mk was in the wrong directory, which caused
    the subordinate Android.mk files to be included twice. In addition,
    setting TOP_DIR was also causing issues, so this was renamed to
    MXTAPP_TOP_DIR.
    
    Change-Id: I59b0e7547a7d54ea9bd22abb1f12531776a42f43

---
+++ Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+MXTAPP_TOP_DIR := $(LOCAL_PATH)
+
+GIT_VERSION = `sh -c '${MXTAPP_TOP_DIR}/build-aux/version.sh'`
+
+subdirs := $(addprefix $(MXTAPP_TOP_DIR)/,$(addsuffix /Android.mk, \
+    src/libmaxtouch \
+    src/jni \
+    src/mxt-app \
+    lib/libusbdroid \
+  ))
+
+include $(subdirs)
+
