5f6821917fa948a535299324c6c58129be910a9e
mgong98 <mksgong@gmail.com>
2019-03-11 09:46:59 -0700

    Redirect ndk-build to top-level Android.mk

--- Android.mk
+++ Android.mk
@@ -1,12 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
-MXTAPP_TOP_DIR := $(LOCAL_PATH)
-
 GIT_VERSION = `sh -c '${MXTAPP_TOP_DIR}/build-aux/version.sh'`
 
 subdirs := $(addprefix $(MXTAPP_TOP_DIR)/,$(addsuffix /Android.mk, \
     src/libmaxtouch \
-    src/jni \
     src/mxt-app \
     lib/libusbdroid \
   ))
