756054a030484cc2442c22ded0553d9b5c7a36e5
Bowe Neuenschwander <bowe.neuenschwander@garmin.com>
2019-03-14 10:55:12 -0500

    Fix AOSP build again
    Fix faulty commit 5f6821917fa948a535299324c6c58129be910a9e
    
    Change-Id: I3c060c343dc3f3cbce84dfb094ae86c0515e228b

--- Android.mk
+++ Android.mk
@@ -1,5 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
+MXTAPP_TOP_DIR := $(LOCAL_PATH)
+
 GIT_VERSION = `sh -c '${MXTAPP_TOP_DIR}/build-aux/version.sh'`
 
 subdirs := $(addprefix $(MXTAPP_TOP_DIR)/,$(addsuffix /Android.mk, \
