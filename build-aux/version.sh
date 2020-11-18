3d5a56f094851bb63afff62ebecc76f94d48ba21
Michael <mksgong@gmail.com>
2020-11-17 21:58:32 -0800

    Update version script

--- build-aux/version.sh
+++ build-aux/version.sh
@@ -5,9 +5,7 @@ TOPDIR=$(dirname "$0")/..
 if [ -z "$VERSION" ]
 then
   VERSION=$(tr -d '\n' < "$TOPDIR/VERSION")
-fi
-
-if [ -d "$TOPDIR/.git" ]
+elif [ -d "$TOPDIR/.git" ]
 then
   VERSION=$(cd "$TOPDIR" && git describe --tags --dirty=-mod --always | sed s/^v//)
 fi
